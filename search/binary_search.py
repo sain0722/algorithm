290c1dc6d3df7f8321adff447e3cc4a6346b30ae
sain0722 <sain0722@naver.com>
2020-06-01 09:07:07 +0900

    binary_search

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,7 +1,8 @@
+# 반복문 사용
 def binary_search(arr, target):
 
     start = 0
-    end = len(arr)
+    end = len(arr)-1
 
     while start < end:
 
@@ -10,7 +11,7 @@ def binary_search(arr, target):
         if arr[middle] == target:
             return middle
 
-        elif arr[middle] > middle:
+        elif arr[middle] > target:
             start = middle + 1
 
         else:
@@ -22,4 +23,23 @@ def binary_search(arr, target):
 arr = [2, 3, 5, 7, 11, 13, 17]
 target = 7
 target_idx = binary_search(arr, target)
+print(target_idx, arr.index(target))
+
+
+# recursive binary search
+def binary_search_recur(arr, target, start, end):
+
+    middle = (start + end) // 2
+
+    if arr[middle] == target:
+        return middle
+
+    elif arr[middle] > target:
+        return binary_search_recur(arr, target, middle+1, end)
+
+    else:
+        return binary_search_recur(arr, target, start, middle-1)
+
+
+target_idx = binary_search_recur(arr, target, 0, len(arr)-1)
 print(target_idx, arr.index(target))
