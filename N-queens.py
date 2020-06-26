81b3c80726c3681580b10433073a6e279566d0c7
sangmin <sain0722@naver.com>
2020-06-26 18:40:32 +0900

    Back Tracking

---
+++ N-queens.py
@@ -0,0 +1,39 @@
+def queens(i):
+
+    global result_list
+    if promising(i):
+        if i == N:
+            for index in range(1, N + 1):
+                result_str = "<{}, {}>".format(index, col[index])
+                result_list.append(result_str)
+
+        else:
+            for j in range(1, N + 1):
+                col[i+1] = j
+                queens(i + 1)
+
+
+def promising(i):
+
+    k = 1
+    switch = True
+
+    while k < i and switch:
+        if col[i] == col[k] or abs(col[i] - col[k]) == i - k:
+            switch = False
+        k += 1
+
+    return switch
+
+
+N = int(input())
+col = [0] * (N+1)
+result_list = []
+
+queens(0)
+
+epoch = len(result_list) // N + 1
+for i in range(1, epoch):
+    print(i, result_list[N * (i-1): N * i])
+
+print("Total Number: ", i)
